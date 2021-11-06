8af327f3275cf8794a90f2eb44c07980e07e8aaa
unknown <facundo.girardi@gmail.com>
2021-11-06 18:25:33 -0300

    Heroku

--- src/controller/webServicesExterno.js
+++ src/controller/webServicesExterno.js
@@ -1,5 +1,5 @@
-//const urlExtApi = "https://iabackend.herokuapp.com/"; // Colocar endpoint de BancoA
-const urlExtApi = "http://localhost:4000/";
+const urlExtApi = "https://iabackend.herokuapp.com/"; // Colocar endpoint de BancoA
+//const urlExtApi = "http://localhost:4000/";
 
 const urlWebServicesExterno = {
   //Endpoints externos
